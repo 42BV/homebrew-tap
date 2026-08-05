12204d3fc5e00a404204ba78583b572968819870
Dennis van der Kraan <dennis.van.der.kraan@42.nl>
2026-08-05 14:37:10 +0200

    normatik: release 0.0.1

--- Formula/normatik.rb
+++ Formula/normatik.rb
@@ -1,22 +1,13 @@
 class Normatik < Formula
   desc "Command-line interface for the Normatik Public API"
   homepage "https://github.com/42BV/normatik-cli"
-  url "https://github.com/42BV/normatik-cli/archive/refs/tags/v0.0.4.tar.gz"
-  sha256 "5493c6264f2f8b0339bc670ab53fe4b4528e9c38590db8c9d6966eff4d1984a5"
+  url "https://github.com/42BV/normatik-cli/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "de35ed03258158d4e7c470b571fddcaf0c1700ca44d244d93dc55c9b695be90a"
   license "MIT"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/42BV/homebrew-tap/releases/download/normatik-0.0.4"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb28bb3a35a385abdb5035ef08bf689d085a33d35264d43ef8f93d17ddc119b7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "531385ff43404be2cde5daf8147b8d7572f95ad13037ef1f0aaa7a14dbde4d7d"
-    sha256 cellar: :any_skip_relocation, tahoe:         "c3805fd5366de50383a6b0b0e7f8c56cfaa36c2f99dd0cfbe9b02dd80c70c8ff"
-    sha256 cellar: :any_skip_relocation, sequoia:       "34cd33f8aac440b42f8f4806f017c976abe15264b9153382e1666ca4780dea2b"
   end
 
   depends_on "go" => :build
