dc33875e12159ade51a79ff471eb89170c732ccc
Dennis van der Kraan <dennis.van.der.kraan@42.nl>
2026-08-05 11:54:54 +0200

    Add source-built Normatik formula

---
+++ Formula/normatik.rb
@@ -0,0 +1,21 @@
+class Normatik < Formula
+  desc "Command-line interface for the Normatik Public API"
+  homepage "https://github.com/42BV/normatik-cli"
+  url "https://github.com/42BV/normatik-cli/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "b9a6077c8194a8a1b7fbcac9f331e35c36ade3b1486f805bfde0cae3cd942e26"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/42BV/normatik-cli/internal/cli.version=#{version}"
+    system "go", "build", "-trimpath", "-ldflags", ldflags, "-o", bin/"normatik", "./cmd/normatik"
+
+    generate_completions_from_executable bin/"normatik", shell_parameter_format: :cobra
+  end
+
+  test do
+    ENV["NORMATIK_CONFIG"] = testpath/"config.toml"
+    assert_match "no environment configured", shell_output("#{bin}/normatik base-url 2>&1", 78)
+  end
+end
