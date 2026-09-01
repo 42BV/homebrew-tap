870da06e2330a66e33046178d90110a6729a968d
Dennis van der Kraan <208939565+DvdK-42@users.noreply.github.com>
2026-09-01 14:10:46 +0000

    normatik: add 1.1.1 bottle.

--- Formula/normatik.rb
+++ Formula/normatik.rb
@@ -10,6 +10,14 @@ class Normatik < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/42BV/homebrew-tap/releases/download/normatik-1.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "18959d38afb90e40579324d68ecb5137ddd22b12ccd1cca5182623105566f472"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a0d184af72276b6ac5db5954b0322a3299acd173bf4639766dad8f058d56275a"
+    sha256 cellar: :any_skip_relocation, tahoe:         "22708e35722d71af5fe9f5cc6c8d25ee49b1eb68976ef754564c0f34398ac081"
+    sha256 cellar: :any_skip_relocation, sequoia:       "679702bb26b91f21aa189fecef837b340b63e15ee7d7e0c092f22092ceac44cb"
+  end
+
   depends_on "go" => :build
 
   def install
