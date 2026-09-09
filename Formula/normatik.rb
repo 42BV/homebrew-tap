7ab36b5384dea7fefa13bfd6ecc93599d22b732e
Dennis van der Kraan <dennis.van.der.kraan@42.nl>
2026-09-09 17:25:10 +0200

    normatik: release 1.3.0

--- Formula/normatik.rb
+++ Formula/normatik.rb
@@ -1,18 +1,13 @@
 class Normatik < Formula
   desc "Command-line interface for the Normatik Public API"
   homepage "https://github.com/42BV/normatik-cli"
-  url "https://github.com/42BV/normatik-cli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "ace00623ab3f83cc1b8167735d522a7386c70ffa92af72c8c2754bdf9ad6cdea"
+  url "https://github.com/42BV/normatik-cli/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "788a4bb5ccd27add548be6338270e3027a74e134f33aa92afdb9ffa01f619dc9"
   license "MIT"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/42BV/homebrew-tap/releases/download/normatik-1.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "06816ab17accbe861534c8b7e32232c037713673f9c0d3cbae3d919f2d17c92b"
   end
 
   depends_on "go" => :build
